1191279a7e50872ca88cff7dbab1bad049232af8
Kautuk <kautukpandey2004@gmail.com>
2026-01-04 11:16:17 +0530

    Only getAllVideos remaining + testing of video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -10,9 +10,7 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
-    // const videos = await Video.find({title: query}).skip(page).limit(limit).sort(sortBy)
-
-    // return res.status(200).json(new ApiResponse(200,videos,"All videos fetched successfully"))
+   
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
