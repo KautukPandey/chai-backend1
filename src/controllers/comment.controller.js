1127f55669dab3b50daa2105f5ffe1978a7c01f7
Kautuk <kautukpandey2004@gmail.com>
2026-01-07 14:46:16 +0530

    Add validation and error handling for comment operations

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -10,6 +10,22 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"Invalid object ID")
+    }
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(400,"Video not found")
+    }
+    const filter = {}
+    if(videoId){
+        filter.video= videoId
+    }
+    const skip = (page-1)*limit
+
+    const allComments = await Comment.find(filter).skip(skip).limit(limit)
+
+    return res.status(200).json(new ApiResponse(200,allComments,"All comments fetched"))
 
 })
 
@@ -39,10 +55,51 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {content} = req.body
+    if(!content || content.trim()===""){
+        throw new ApiError(400,"Content cannot be empty")
+    }
+    const {commentId} = req.params
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400,"Invalid ID")
+    }
+    const comment = await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(400,"Comment does not exists")
+    }
+    
+    if(!comment.owner.equals(req.user._id)){
+        throw new ApiError(400,"Not the owner")
+    }
+    const comment1 = await Comment.findByIdAndUpdate(commentId,
+        {
+            $set:{
+                content,
+            }
+        },
+        {new: true}
+    )
+
+    return res.status(200).json(new ApiResponse(200,comment1,"Comment updated"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400,"Invalid ID")
+    }
+    const comment = await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(400,"Comment does not exits")
+    }
+    if(!comment.owner.equals(req.user._id)){
+        throw new ApiError(400,"Not the comment owner")
+    }
+    const comment1 = await Comment.findByIdAndDelete(commentId)
+
+    return res.status(200).json(new ApiResponse(200,comment1,"Comment Deleted"))
+
 })
 
 export {
